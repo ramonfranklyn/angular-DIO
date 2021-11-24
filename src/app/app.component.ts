908aa82983a4ef7927407411c22abc7ff3c1632f
Ramon <ramon.ladeia@agu.gov.br>
2021-11-24 10:48:09 -0300

    projeto da live diretivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-first-app';
+export class AppComponent implements OnInit{
+
+  count = 0;
+
+  constructor(){
+
+  }
+
+  ngOnInit(){
+    let interval = setInterval(() => {
+      this.count++;
+      if(this.count === 10){
+        clearInterval(interval);
+      }
+    }, 1000)
+  }
+
+
 }
